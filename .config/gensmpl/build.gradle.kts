f47f5b661a84a0774366ea7f25019d5b189c68e0
Mezentsev Denis <dmezentsev@marathonbet.ru>
2021-01-03 21:42:24 +0300

    New version

--- .config/gensmpl/build.gradle.kts
+++ .config/gensmpl/build.gradle.kts
@@ -5,3 +5,10 @@ repositories { jcenter() }
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
+
+val jar by tasks.getting(org.gradle.jvm.tasks.Jar::class) {
+    manifest {
+        attributes["Main-Class"] = "mezlogo.basic_piocli_kts.MainKt"
+    }
+    from(configurations["runtimeClasspath"].map { if (it.isDirectory) it else zipTree(it) })
+}
