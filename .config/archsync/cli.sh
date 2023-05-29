ad97d900bc23512c8b446b8bf4d13f65c2f3867a
Mezentsev Denis <mezentsev.d@olytechspb.com>
2023-05-29 11:15:27 +0300

    New version

--- .config/archsync/cli.sh
+++ .config/archsync/cli.sh
@@ -1,1 +1,30 @@
+#!/bin/env bash
+
+echo "init ssh-daemon user service"
 systemctl --user enable --now ssh-agent.service
+
+echo "init tmux plugins"
+if [[ "$TMUX" ]]; then
+    /usr/share/tmux-plugin-manager/scripts/install_plugins.sh
+    /usr/share/tmux-plugin-manager/scripts/update_plugin.sh all
+fi
+
+echo "init default shell"
+current_shell="$(cat /etc/passwd | grep $USER | cut -d ':' -f7)"
+zsh_shell="$(which zsh)"
+if [ "$current_shell" != "$zsh_shell" ]; then
+    echo "current shell: [$current_shell] is not zsh: [$zsh_shell]"
+    chsh -s /usr/bin/zsh
+fi
+sudo ps
+
+echo "init download dirs"
+mkdir -p ~/downloads
+ln -Tsf ~/downloads ~/Downloads
+ln -Tsf ~/downloads ~/Desktop
+ln -Tsf ~/downloads ~/Documents
+
+echo "add groups"
+sudo gpasswd -a $USER libvirt
+sudo gpasswd -a $USER wireshark
+sudo gpasswd -a $USER docker
