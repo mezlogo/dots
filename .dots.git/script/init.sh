7ef88e75402cd749618877cd39421f9db0d8302e
Mezentsev Denis <mezentsev.d@olytechspb.com>
2023-05-29 11:09:05 +0300

    new

---
+++ .dots.git/script/init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "clone dots"
+DOTS_FROM="$HOME/repos/dots"
+DOTS_TO="$HOME/.dots.git"
+if [[ ! -d "$DOTS_TO" ]]; then
+    mkdir -p "$DOTS_TO"
+    mv $DOTS_FROM/.git "$DOTS_TO"
+    cp -vRn $DOTS_FROM/* $HOME
+fi
+
+echo "config .dots.git's url"
+git -C ~/.dots.git remote set-url origin git@github.com:mezlogo/dots.git
