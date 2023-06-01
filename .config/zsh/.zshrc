3fa9f16e8b6de7a75ab90307e7467cd2e5308927
Mezentsev Denis <mezentsev.d@olytechspb.com>
2023-06-01 16:23:07 +0300

    New version

--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -2,9 +2,9 @@ source "$ZDOTDIR/aliases.sh"
 source "$ZDOTDIR/zshaliases.sh"
 source "$ZDOTDIR/history.sh"
 source "$ZDOTDIR/options.sh"
+source "$ZDOTDIR/plugins.sh"
 source "$ZDOTDIR/keybinds.sh"
 source "$ZDOTDIR/prompt.sh"
-source "$ZDOTDIR/plugins.sh"
 
 stty -ixon -ixoff
 [ -e "$EXO/config/shell/sourceitrc.sh" ] && source "$EXO/config/shell/sourceitrc.sh"
--- .config/zsh/plugins.sh
+++ .config/zsh/plugins.sh
@@ -19,3 +19,23 @@ function load_plugins() {
 export ZSH_AUTOSUGGEST_USE_ASYNC="true"
 export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
 load_plugins
+
+# FZF BLOCK
+
+source /usr/share/fzf/completion.zsh
+
+# Options to fzf command
+export FZF_COMPLETION_OPTS='--border --info=inline'
+
+# Use fd (https://github.com/sharkdp/fd) instead of the default find
+# command for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
